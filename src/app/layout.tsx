47ca40d71ace8bf88fb801835de920d72c6537e8
alisa <alisa@alisaqaq.moe>
2024-05-16 00:34:48 +0800

    fix: wide screen layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from 'next'
 
 import '@/styles/index.css'
 
-import { Terminal } from 'lucide-react'
-
 import { Providers } from '@/components/providers/providers'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import cfg from '@/config'
@@ -29,9 +27,8 @@ export default function RootLayout({
         <Providers>
           <div className="relative h-full max-h-screen overflow-auto p-4 font-sans">
             <Alert>
-              <Terminal className="h-4 w-4" />
-              <AlertTitle>ATTENTION</AlertTitle>
-              <AlertDescription>🚧 本站仍在开发中</AlertDescription>
+              <AlertTitle>🚧 ATTENTION</AlertTitle>
+              <AlertDescription>本站仍在开发中</AlertDescription>
             </Alert>
             {children}
           </div>
