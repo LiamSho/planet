6cd51a25b380c7b689e859fd148319e8deeccd3b
alisa <alisa@alisaqaq.moe>
2024-05-29 21:41:06 +0800

    fix: add custom not-found page to resovle cloudflare edge runtime issue

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+export const runtime = 'edge'
+
+const NotFoundPage: FC = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+    </div>
+  )
+}
+
+export default NotFoundPage
