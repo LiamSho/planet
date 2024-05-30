4e1ef87a1a3ce3831782dc383a0ab7998b1f7529
alisa <alisa@alisaqaq.moe>
2024-05-31 00:23:25 +0800

    fix: remove ContentContainer

--- src/app/dev/markdown/page.tsx
+++ src/app/dev/markdown/page.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import { DevMarkdown } from '@/components/dev'
-import { ContentContainer } from '@/components/layout/container'
 
 export default function DevMarkdownPage() {
   return (
     <>
-      <ContentContainer>
-        <DevMarkdown />
-      </ContentContainer>
+      <DevMarkdown />
     </>
   )
 }
