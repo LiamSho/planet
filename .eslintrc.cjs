dd3599098caa305938e0659cf9d5cd2905e9b85b
alisa <alisa@alisaqaq.moe>
2024-05-31 09:54:53 +0800

    feat: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
 }
---
+++ src/stories/ui/theme-switch.stories.ts
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { ThemeSwitch } from '@/components/ui/theme-switch'
+
+const meta: Meta = {
+  component: ThemeSwitch,
+}
+
+export default meta
+
+export const Default: StoryObj = {}
